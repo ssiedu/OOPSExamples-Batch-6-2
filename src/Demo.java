c764d90ab863a5e58c9163fd09fe91ce1c0352a6
manoj <manojs121@rediffmail.com>
2021-04-26 18:08:47 +0530

    260421

--- src/Demo.java
+++ src/Demo.java
@@ -1,37 +1,29 @@
 
 public class Demo {
-	int x, y;			//instance variables (non-static variables)
-	static int p, q;	//class variables (static variables)
+	private int x, y;			
+	
+	public Demo(int a, int b) {x=a; y=b;}
+	public void print() {
+		System.out.println(x+","+y);
+	}
 	
 	public static void main(String args[]) {
 		
-		Demo d1=new Demo();	//1st instance
-		Demo d2=new Demo();	//2nd instance
-		Demo d3=new Demo();	//3rd instance
+		Demo d1;	//just declaring a reference
+		new Demo(10,20);	//creating a object (not storing it to a ref) (eligible for GC)
+		Demo d2=new Demo(30,40); //creating a ref and object both and assigning adr to d2
 		
-		d1.x=10; d1.y=20;
-		d2.x=30; d2.y=40;
-		d3.x=50; d3.y=60;
-		
-		System.out.println("Members of  d1 : "+d1.x+","+d1.y);
-		System.out.println("Members of  d2 : "+d2.x+","+d2.y);
-		System.out.println("Members of  d3 : "+d3.x+","+d3.y);
-		System.out.println("_________________________________________________");
-		
-		d1.p=11; d1.q=12;
-		d2.p=13; d2.q=14;
-		d3.p=15; d3.q=16;
-		
-		System.out.println("Class Members Using d1 : "+d1.p+","+d1.q);
-		System.out.println("Class Members Using d2 : "+d2.p+","+d2.q);
-		System.out.println("Class Members Using d3 : "+d3.p+","+d3.q);
-		
-		
-		
-		
-		
-		
-		
+		//d2.print();
+		Demo d3=d2;		//assigning the address stored in d2 to a new reference d3 (now d2 and d3 both points to same object)
+		d1=d2;			//assigning the address stored in d2 to d1
+		d1.print();
+		d2.print();
+		d3.print();
+		System.out.println("_____________________________________________");
+		d3=new Demo(50,60);
+		d1.print();
+		d2.print();
+		d3.print();
 		
 		
 		
