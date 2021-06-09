c82aab3a509a69a90555aae9d6f7f1a2a94219a1
manoj <manojs121@rediffmail.com>
2021-06-09 17:55:56 +0530

    090621

---
+++ src/p1/ChildWithInPkg.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class ChildWithInPkg extends Main {
+
+	public void show() {
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		//System.out.println(d);
+	}
+	
+}
